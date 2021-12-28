94e15118b024d916b7687a995b55e89f58f9a5ea
Saroneh <d.saroneh@gmail.com>
2021-12-28 12:17:11 +0100

    small change to list

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -9,9 +9,9 @@ interface Props { //
 
 export default function ProductList({products}: Props) {
 return (
-    <Grid container spacing={4}>
+    <Grid container spacing={4} >
         {products.map(product => (
-            <Grid item xs={3}>
+            <Grid item xs={3} key={product.id}>
             <ProductCard key={product.id} product={product} />
             </Grid>
         ))}
