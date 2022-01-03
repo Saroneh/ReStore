d306e14902798650795a9ec7225e776a68844056
Saroneh <d.saroneh@gmail.com>
2022-01-04 00:26:02 +0100

    end of section 4

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -2,6 +2,7 @@
 import { Badge, AppBar, Box, IconButton, List, ListItem, Switch, Tab, Tabs, Toolbar, Typography } from '@mui/material';
 import { ShoppingCart} from '@mui/icons-material';
 import { NavLink } from 'react-router-dom';
+import { display } from '@mui/system';
 
 
 interface Props {
@@ -23,46 +24,68 @@ const rightLinks =
     { title: 'register', path:'/register/' },  
 ]
 
+const navStyle =
+{
+    color: 'inherit',
+    typography: 'h6',
+    display: 'flex',
+    textDecoration: 'none',
+    '&:hover': {
+    color: 'secondary.main',
+    typography: 'h4'},
+    '&.active': {
+    color: 'text.secondary'},
+    }
+
 export default function Header({darkMode, handleChange}:Props) {
 
     return(
         <AppBar position ='static' sx={{mb: 4}}>
-            <Toolbar>
-                <Typography variant='h6'>Re-Store</Typography>
+            <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
 
+
+                <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <Typography variant='h6'
+                    component={NavLink} 
+                    to =  '/'
+                    exact
+                    sx = {navStyle}>
+                    Re-Store</Typography>
                 <Switch checked={darkMode} onChange={handleChange} color='secondary'/>
+                </Box>
                 
-                
-                <List sx= {{display: 'flex'}}>
+                <Box>
+                <List sx= {navStyle}>
                     {midLinks.map(({title,path}) => (
                   <ListItem 
                     component={NavLink} 
                     to = {path}
                     key={path}
-                    sx = {{color: 'inherit',typography: 'h6'}}>
+                    sx = {navStyle}>
                       {title.toUpperCase()}
                  </ListItem>                
                     )) }
                 </List>
+                </Box>
 
+                <Box sx={{display: 'flex', alignItems: 'center'}}>
                 <IconButton size='large' sx={{color: 'inherit'}}>
                     <Badge badgeContent={4} color='secondary'>
                     <ShoppingCart/>
                     </Badge>
                 </IconButton>
-
-                <List sx= {{display: 'flex'}}>
+                <List sx= {navStyle}>
                     {rightLinks.map(({title,path}) => (
                   <ListItem 
                     component={NavLink} 
                     to = {path}
                     key={path}
-                    sx = {{color: 'inherit',typography: 'h6'}}>
+                    sx = {navStyle}>
                       {title.toUpperCase()}
                  </ListItem>                
                     )) }
                 </List>
-               
+                </Box>
         
 
             </Toolbar>
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,12 +1,48 @@
-import { Typography } from "@mui/material";
+import { Divider, Grid, Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Product } from "../../app/models/poduct";
 
 
 export default function ProductDetails() {
+    const {id} = useParams<{id: string}>();
+    const [product, setProduct] = useState<Product | null>(null);
+    const [loading, setLoading] = useState(true);
+
+
+
+    useEffect(() => {
+
+        axios.get(`https://localhost:5001/api/Products/${id}`)
+            .then(response => setProduct(response.data))
+            .catch(error => console.log("error"))
+            .finally(() => setLoading(false));
+
+    }, [id]) //specified dependency. useEffect call when component mount or when parameter depency changed. 
+    //if dependency is empty it will only be mounted once.
+
+
+    if (loading) return <h3>Loading...</h3>
+ 
+    if (!product) return <h3>Product not found</h3>
 
     return(
-        <Typography variant='h2'>
-            Product details
-        </Typography>
-    )
+
+
+        <Grid container spacing={6}>
+            <Grid item xs= {6}>
+                <img src ={product.pictureUrl} alt={product.name} style={{width:'100%'}} />
+            </Grid>
+            <Grid item xs={6}>
+            <Typography variant='h3'>{product.name}</Typography>
+            <Divider sx = {{mb:2}}/>
+            <Typography variant='h6' color='secondary'>${(product.price / 100).toFixed(2)}</Typography>
+            </Grid>     
+
+        </Grid>
+
+    );
+
     
 }
